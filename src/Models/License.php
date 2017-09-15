91ba6bcd64fcefa6f76b2f0c73d40f2c5a456e18
Tyler Arbon <tylercd100@gmail.com>
2017-09-15 12:13:50 -0600

    fixed missing use statement

--- src/Models/License.php
+++ src/Models/License.php
@@ -2,6 +2,7 @@
 
 namespace Tylercd100\License\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Tylercd100\License\Interfaces\License as ILicense;
 
 class License extends Model implements ILicense
