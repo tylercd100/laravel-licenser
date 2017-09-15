2a00c143353b7e2456faba83ae96837449b63bff
Tyler Arbon <tylercd100@gmail.com>
2017-09-15 12:19:56 -0600

    Removed license interface reference

--- src/Models/License.php
+++ src/Models/License.php
@@ -3,9 +3,8 @@
 namespace Tylercd100\License\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Tylercd100\License\Interfaces\License as ILicense;
 
-class License extends Model implements ILicense
+class License extends Model
 {
     protected $table = "licenses";
 }
